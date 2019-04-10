fc8b01431b99c1e011345fdf239b06069bd507a0
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-10 23:58:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, {[key]: value}, object)
 }
-function destructivelyUpdateObjectWithkeyAndValue (object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   object[key] = value
   return object
 }
