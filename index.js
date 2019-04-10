1d3cc4819c0167ff232736157158380859f11457
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-10 23:40:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, {key: value}, object)
+  return Object.assign({}, {[key]: value}, object)
 }
