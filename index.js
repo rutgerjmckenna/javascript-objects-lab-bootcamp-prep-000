68df0ece34209cc8882633ecf572a7a314e92cfc
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-11 00:01:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, {[key]: value}, object)
 }
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  object.key = value
+  object[key] = value
   return object
 }
