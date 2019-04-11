8ba47b16998a38d7f7648fbbe54064b759edc50b
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-11 00:03:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key, value) {
-  delete 
+  return delete object.key
 }
